db029c3f4ae8348a016cc26d33486c318af7e9f7
tanjz <tanjunze@wzdai.com>
2018-04-28 16:01:04 +0800

    添加动画路由，省略new关键字

--- meiju_app/lib/page/routes/routes.dart
+++ meiju_app/lib/page/routes/routes.dart
@@ -2,23 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:meiju_app/page/MoiveDetailPage.dart';
 import 'package:meiju_app/page/MoiveClassifyPage.dart';
 import 'package:meiju_app/page/TakePhotoPage.dart';
+import 'package:meiju_app/page/WebViewPage.dart';
 
 /// 电影详情页面路由
-const  moiveDetailRoute = "/moive_detail";
+const moiveDetailRoute = "/moive_detail";
 
 /// 电影分类页面路由
 const moiveClassifyRoute = "/moive_classify";
 
-///
+/// 自定义相机
 const takePhotoRoute = "/take_photo";
 
-
-Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  moiveDetailRoute: (BuildContext context) => new MoiveDetial(),
-  moiveClassifyRoute: (BuildContext context) => new MoiveClassifyPage(),
-  takePhotoRoute:(BuildContext context) => new TakePhotoPage(),
+Map<String, Widget> pages = <String, Widget>{
+  moiveDetailRoute: MoiveDetial(),
+  moiveClassifyRoute: MoiveClassifyPage(),
+  takePhotoRoute: TakePhotoPage()
 };
 
-openPage(BuildContext context,String routeName){
-  Navigator.pushNamed(context, routeName);
+Map<String, WidgetBuilder> routes = pages.map((name, page) {
+  return MapEntry(name, (BuildContext context) => page);
+});
+
+openPage(BuildContext context, String routeName, {bool useDefault=false}) {
+  if (useDefault) {
+    Navigator.pushNamed(context, routeName);
+  } else {
+    Navigator.push(context, buildAnimRoute(pages[routeName]));
+  }
+}
+
+openWebView(BuildContext context, String url) {
+  Navigator.push(context, buildAnimRoute(new WebViewPage(url)));
+}
+
+buildAnimRoute(Widget page) {
+  return PageRouteBuilder(
+      opaque: false,
+      pageBuilder: (BuildContext context, _, __) {
+        return page;
+      },
+      transitionsBuilder:
+          (___, Animation<double> animation, ____, Widget child) {
+        return new FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      });
+}
+
+buildMaterialRoute(Widget page) {
+  return MaterialPageRoute(builder: (context) => page);
 }
