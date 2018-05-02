aa2d69e1810fb9f6d79c0b68725052ea0e871795
tanjz <tanjunze@wzdai.com>
2018-05-02 14:13:01 +0800

    添加路由

---
+++ flutter_compt/lib/page/routes.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_compt/page/roll_page.dart';
+
+/// 循环图片
+const LoopViewRoute = "/loopView";
+
+
+Map<String, Widget> pages = <String, Widget>{
+  LoopViewRoute: RollImagePage(),
+};
+
+Map<String, WidgetBuilder> routes = pages.map((name, page) {
+  return MapEntry(name, (BuildContext context) => page);
+});
+
+openPage(BuildContext context, String routeName, {bool useDefault=false}) {
+  if (useDefault) {
+    Navigator.pushNamed(context, routeName);
+  } else {
+    Navigator.push(context, buildAnimRoute(pages[routeName]));
+  }
+}
+
+buildAnimRoute(Widget page) {
+  return PageRouteBuilder(
+      opaque: false,
+      pageBuilder: (BuildContext context, _, __) {
+        return page;
+      },
+      transitionsBuilder:
+          (___, Animation<double> animation, ____, Widget child) {
+        return new FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      });
+}
+
+buildMaterialRoute(Widget page) {
+  return MaterialPageRoute(builder: (context) => page);
+}
