39f4fb8a73e9c0dfb8951e853f95335f17235d42
tanjz <tanjunze@wzdai.com>
2018-05-02 14:14:44 +0800

    添加实例索引

---
+++ flutter_compt/lib/page/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_compt/page/routes.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("自定义小组件集合"),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            onTap: () {
+              openPage(context, LoopViewRoute);
+            },
+            title: new Text("轮番图小组件"),
+          ),
+          new ListTile(
+            title: new Text("时间/地区选择器"),
+          )
+        ],
+      ),
+    );
+  }
+}
