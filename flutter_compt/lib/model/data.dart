1be1854e9e724f98974ae12255112850bc8c44e0
tanjz <tanjunze@wzdai.com>
2018-05-02 14:12:51 +0800

    添加测试数据

---
+++ flutter_compt/lib/model/data.dart
@@ -0,0 +1,110 @@
+class ImageBean {
+  ImageBean({this.name, this.url});
+  final String name;
+  final String url;
+}
+
+final itemBeans = <ImageBean>[
+  new ImageBean(
+      name: "此时此刻第一季",
+      url: 'http://img.kukan5.com:808/pic/uploadimg/2018-2/5a81b6eeb9348.jpg'),
+  new ImageBean(
+      name: "高中卧底第一季",
+      url:
+          'http://img.kukan5.com:808/pic/uploadimg/2018-1/d813964dac7f640159c475447de2b1c8.jpg'),
+  new ImageBean(
+      name: "住院医师第一季",
+      url: 'http://img.kukan5.com:808/pic/uploadimg/2018-1/p2506610722.jpg'),
+  new ImageBean(
+      name: "相对宇宙第一季",
+      url: 'http://img.kukan5.com:808/pic/uploadimg/2017-12/5a3212e3557f8.jpg'),
+  new ImageBean(
+      name: "仁医第一季",
+      url: 'http://img.kukan5.com:808/pic/uploadimg/2017-9/p2496064819.jpg'),
+];
+
+class MoveBean {
+  MoveBean({this.name, this.img, this.type, this.classify, this.moveNum});
+  final String name;
+  final String img;
+  final String type;
+  final String classify;
+  final moveNum;
+}
+
+//2018新剧推荐
+final recommendBeans = <MoveBean>[
+  new MoveBean(
+      name: "洛城到赌城第一季",
+      img: "http://img.kukan5.com:808/pic/uploadimg/2017-12/p2506694197.jpg",
+      type: "都市/情感 /",
+      classify: "喜剧,剧情",
+      moveNum: "第9集"),
+  new MoveBean(
+      name: "姬芮第一季",
+      img:
+          "http://img.kukan5.com:808/pic/uploadimg/2018-3/20183111815481844.jpg",
+      type: "犯罪/历史 / ",
+      classify: "迷你剧,悬疑,剧情",
+      moveNum: "第2集"),
+  new MoveBean(
+      name: "黑霹雳第一季",
+      img:
+          "http://img.kukan5.com:808/pic/uploadimg/2018-3/20183111815481844.jpg",
+      type: "魔幻/科幻 /",
+      classify: "科幻",
+      moveNum: "第9集"),
+  new MoveBean(
+      name: "青春崛起第一季",
+      img:
+          "http://img.kukan5.com:808/pic/uploadimg/2018-3/20183111815481844.jpg",
+      type: "都市/情感 / ",
+      classify: "歌舞,喜剧",
+      moveNum: "第2集"),
+  new MoveBean(
+      name: "住院医师第一季",
+      img: "http://img.kukan5.com:808/pic/uploadimg/2018-1/p2506610722.jpg",
+      type: "都市/情感 / ",
+      classify: "医务,剧情",
+      moveNum: "第8集"),
+  new MoveBean(
+      name: "沉默的天使第一季",
+      img: "http://img.kukan5.com:808/pic/uploadimg/2018-1/5a672335d5228.jpg",
+      type: "犯罪/历史 / ",
+      classify: "罪案,剧情",
+      moveNum: "第9集"),
+  new MoveBean(
+      name: "护儿使者第一季",
+      img: "http://img.kukan5.com:808/pic/uploadimg/2018-1/p2506610722.jpg",
+      type: "都市/情感 / ",
+      classify: "喜剧",
+      moveNum: "第2集"),
+  new MoveBean(
+      name: "高中卧底第一季",
+      img: "http://img.kukan5.com:808/pic/uploadimg/2018-1/p2506610722.jpg",
+      type: "选秀/综艺 / ",
+      classify: "记录,剧情",
+      moveNum: "第9集")
+];
+//2018新剧推荐
+final recommendBeans2 = <MoveBean>[
+  new MoveBean(
+      name: "洛城到赌城第一季",
+      img: "http://img.kukan5.com:808/pic/uploadimg/2017-12/p2506694197.jpg",
+      type: "都市/情感 /",
+      classify: "喜剧,剧情",
+      moveNum: "第9集"),
+  new MoveBean(
+      name: "姬芮第一季",
+      img:
+          "http://img.kukan5.com:808/pic/uploadimg/2018-3/20183111815481844.jpg",
+      type: "犯罪/历史 / ",
+      classify: "迷你剧,悬疑,剧情",
+      moveNum: "第2集"),
+  new MoveBean(
+      name: "高中卧底第一季",
+      img: "http://img.kukan5.com:808/pic/uploadimg/2018-1/p2506610722.jpg",
+      type: "选秀/综艺 / ",
+      classify: "记录,剧情",
+      moveNum: "第9集"),
+];
