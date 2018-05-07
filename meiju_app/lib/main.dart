bc5b387f1302e2618423d89861b04dc8f21de8b3
tanjz <tanjunze@wzdai.com>
2018-05-07 15:58:07 +0800

    修改示例app名称

--- meiju_app/lib/main.dart
+++ meiju_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: '美剧天堂',
+      title: '美剧APP',
       theme: new ThemeData(
         primarySwatch: Colors.red,
       ),
